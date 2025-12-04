fb6c9fe7b998b4c6e483c2dde5054fba9d634817
João Pedro Lourenço <lourencodossantosjoaopedro0@gmail.com>
2025-12-04 01:20:23 -0300

    feat: informações adicionadas ao dashboard, valor total dos pedidos e quantidade produtos vendidos.

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFetchData } from "../hooks/useFetchData";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const {
@@ -9,20 +10,72 @@ const Home = () => {
   } = useFetchData("https://fakestoreapi.com/carts");
   const { data: produtos } = useFetchData("https://fakestoreapi.com/products");
 
-  const findProduto = (idProdutoPedido) => {
-    const produto = produtos.find((prd) => prd.id == idProdutoPedido);
-    return produto ? `${produto.price}` : "";
+  const [subTotal, setSubTotal] = useState(0);
+
+  const handlePrecos = () => {
+    return pedidos.reduce((acc, pedido) => {
+      const valorTotalPedido = pedido.products.reduce((total, item) => {
+        const produto = produtos.find((prd) => prd.id === item.productId);
+
+        return total + produto.price * item.quantity;
+      }, 0);
+      return acc + valorTotalPedido;
+    }, 0);
   };
 
+  const qtdProdutosVendidos = () => {
+    let total = 0;
+    pedidos.map((pedido) => {
+      pedido.products.map((item) => {
+        total += item.quantity;
+      });
+    });
+
+    return total;
+  };
+
+  const totalProdutos = qtdProdutosVendidos();
+
+  const valorTotal = handlePrecos();
+  console.log(valorTotal);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {pedidos.map((pedido) =>
-        pedido.products.map((item) => (
-          <p>{findProduto(item.productId) * item.quantity}</p>
-        ))
-      )}
+    <>
+      <h1 className="my-5 mx-12 text-3xl font-bold">Pedidos</h1>
+    <div className="w-full max-w-[90%] ml-12 my-5">
+
+      <div className="w-full flex items-center justify-center gap-5 my-5">
+        <div class="card-info">
+          <div class="card-block">
+            <i
+              class="fa-solid fa-dollar-sign"
+              style={{fontSize: "35px", opacity: "0.8"}}
+            ></i>
+          </div>
+
+          <div class="card-text">
+            <h1>Valor total em pedidos</h1>
+            <p>R${valorTotal.toFixed(2)}</p>
+            <Link to="/pedidos" className="link-carts">Ver pedidos</Link>
+          </div>
+        </div>
+        <div class="card-info">
+          <div class="card-block">
+            <i
+              class="fa-solid fa-shopping-cart"
+              style={{fontSize: "35px", opacity: "0.8"}}
+            ></i>
+          </div>
+
+          <div class="card-text">
+            <h1>Qtd. total de produtos vendidos</h1>
+            <p>{totalProdutos}</p>
+            <Link to="/pedidos"  className="link-carts">Ver pedidos</Link>
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   );
 };
 
--- src/Routes/Produtos.jsx
+++ src/Routes/Produtos.jsx
@@ -3,10 +3,31 @@ import { Link } from "react-router-dom";
 import { useFetchData } from "../hooks/useFetchData";
 
 const Produtos = () => {
-  const {data: produtos, loading, error} = useFetchData("https://fakestoreapi.com/products")
+  const {
+    data: produtos,
+    loading,
+    error,
+  } = useFetchData("https://fakestoreapi.com/products");
+
+  const [cart, setCart] = useState(null);
+
+  const addToCart = (produtoId) => {
+    setCart((prevCart) => [prevCart, { ...produtoId }]);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openCart = () => {
+    setIsOpen(!isOpen);
+  };
+
+  console.log(cart);
 
   return (
     <div className="w-full h-full flex items-center justify-center flex-wrap gap-4 my-5">
+      <button onClick={openCart}>
+        <i className="fa-solid fa-shopping-cart"></i>
+      </button>
       {loading ? (
         <p>Carregando...</p>
       ) : (
@@ -18,9 +39,23 @@ const Produtos = () => {
             <p>{produto.category}</p>
             <p>{produto.title}</p>
             <p>${produto.price.toFixed(2)}</p>
+            <button onClick={() => addToCart(produto)}>
+              Adicionar ao carrinho
+            </button>
             <Link to={`/produtos/${produto.id}`}>Ver detalhes</Link>
           </div>
         ))
+      )}
+
+      {isOpen && (
+        <div className="absolute w-[200px] h-full bg-gray-800 right-[0%]">
+          {cart.length > 0 ? (cart.map((item) => (
+              <div>
+                <p>{item.title}</p>
+              </div>
+            ))) : ""}
+            
+        </div>
       )}
     </div>
   );
